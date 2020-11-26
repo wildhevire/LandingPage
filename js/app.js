abdb8b11839b7085c3c08fdc58710170af250158
wildhevire <wildhevire@gmail.com>
2020-11-27 02:20:44 +0700

    tinggal polish

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(".owl-carousel").owlCarousel({
     autoplay:true,
     autoplayTimeout:3000,
     autoplayHoverPause:true,
+    URLhashListener:true,
     navText: [
       '<i class="fa fa-angle-left" aria-hidden="true"></i>',
       '<i class="fa fa-angle-right" aria-hidden="true"></i>',
@@ -16,4 +17,12 @@ $(".owl-carousel").owlCarousel({
     },
 });
 
-AOS.init();
+AOS.init({
+  once:true
+});
+
+$(".carousel-link").click(ToTop);
+function ToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
